a4bab796977759b1b0ca06efeb88f0761a417265
Jakub Filipek <jakubflpk@gmail.com>
2021-05-13 12:07:53 -0700

    smaller model, headles fix and searching code

--- examples/search_dqn.py
+++ examples/search_dqn.py
@@ -1,3 +1,7 @@
+import logging
+logging.basicConfig(filename='search.log', level=logging.CRITICAL)
+
+
 import argparse
 from typing import List
 
@@ -11,9 +15,11 @@ from ray.rllib.models import ModelCatalog
 from ray.rllib.models.torch.torch_modelv2 import TorchModelV2
 import torch
 from torch import nn
+import numpy as np
 
-import plotter
-
+import pandas as pd
+import os
+from tqdm import trange
 
 class ImageCritic(nn.Module):
     def __init__(self):
@@ -33,35 +39,29 @@ class ImageCritic(nn.Module):
         self.common = nn.Sequential(
             nn.Conv2d(3, 8, 3),
             nn.Conv2d(8, 16, 3),
+            nn.MaxPool2d(4),
+            nn.ReLU(),
+            nn.Conv2d(16, 32, 3),
             nn.MaxPool2d(2),
             nn.ReLU(),
-            nn.Conv2d(16, 16, 3),
+            nn.Conv2d(32, 32, 3),
+            nn.MaxPool2d(4),
+            nn.ReLU(),
+            nn.Conv2d(32, 64, 3),
             nn.MaxPool2d(2),
             nn.ReLU(),
-            nn.Conv2d(16, 16, 3),
-            nn.MaxPool2d(2),
-            nn.ReLU(),
-            nn.Conv2d(16, 16, 3),
-            nn.MaxPool2d(2),
-            nn.ReLU(),
-            nn.Conv2d(16, 16, 3),
-            nn.MaxPool2d(2),
-            nn.ReLU(),
-            nn.Conv2d(16, 16, 3),
-            nn.MaxPool2d(2),
-            nn.ReLU(),
-            nn.Conv2d(16, 16, 3),
+            nn.Conv2d(64, 64, 3),
             nn.MaxPool2d(2),
             nn.ReLU(),
             nn.Flatten(),
         )
         self.critic = nn.Sequential(
-            nn.Linear(48, 64),
+            nn.Linear(384, 64),
             nn.ReLU(),
             nn.Linear(64, 1),
         )
         self.agent = nn.Sequential(
-            nn.Linear(48, 64),
+            nn.Linear(384, 64),
             nn.ReLU(),
             nn.Linear(64, 3),
         )
@@ -101,17 +101,81 @@ class RLLibDQNCritic(TorchModelV2, nn.Module):
         return self._value
 
 
-def train_model(args):
-    # We are using custom model and environment, which need to be registered in ray/rllib
-    # Names can be anything.
-    register_env("DuckieTown-MultiMap", lambda _: DiscreteWrapper(MultiMapEnv()))
-
+def train_model(args, config):
     # Define trainer. Apart from env, config/framework and config/model, which are common among trainers.
     # Here is a list of default config keys/values: https://docs.ray.io/en/master/rllib-training.html#common-parameters
     # For DQN specifically there are also additionally these keys: https://docs.ray.io/en/master/rllib-algorithms.html#dqn
     trainer = DQNTrainer(
         env="DuckieTown-MultiMap",
-        config={
+        config=config,
+    )
+
+    # Start training from a checkpoint, if available.
+    if args.model_path:
+        trainer.restore(args.model_path)
+
+    best_mean_reward = -np.inf
+    epoch_of_best_mean_reward = 0
+    path_of_best_mean_reward = None
+
+    for i in trange(args.epochs, desc="Epochs", leave=False):  # Number of episodes (basically epochs)
+        # print(f'----------------------- Starting epoch {i} ----------------------- ')
+        # train() trains only a single episode
+        result = trainer.train()
+        # print(result)
+
+        # Save model so far.
+        checkpoint_path = trainer.save()
+        # print(f'Epoch {i}, checkpoint saved at: {checkpoint_path}')
+
+        if result["episode_reward_mean"] > best_mean_reward:
+            best_mean_reward = result["episode_reward_mean"]
+            epoch_of_best_mean_reward = i
+            path_of_best_mean_reward = checkpoint_path
+
+
+        # Cleanup CUDA memory to reduce memory usage.
+        torch.cuda.empty_cache()
+        # Debug log to monitor memory.
+        print(torch.cuda.memory_summary(device=None, abbreviated=False))
+
+    return best_mean_reward, epoch_of_best_mean_reward, path_of_best_mean_reward
+
+
+def get_parser():
+    parser = argparse.ArgumentParser()
+
+    # Training options
+    parser.add_argument('--epochs', default=100, type=int, help='Num of epochs to train for')
+    parser.add_argument('--n_searches', default=50, type=int, help='Num of epochs to train for')
+
+    # Evaluation options
+    parser.add_argument('--map', default='loop_empty', help='Which map to evaluate on, see https://git.io/J3jES')
+    # Looks like, e.g. `DQN_DuckieTown-MultiMap_2021-05-10_20-43-32ndgfiq44/checkpoint_000009/checkpoint-9`
+    parser.add_argument('--model_path', default='', help='Location to pre-trained model')
+
+    return parser.parse_args()
+
+
+if __name__ == '__main__':
+    args = get_parser()
+
+    # Start ray
+    ray.init()
+    ModelCatalog.register_custom_model("image-dqn", RLLibDQNCritic)
+
+    # We are using custom model and environment, which need to be registered in ray/rllib
+    # Names can be anything.
+    register_env("DuckieTown-MultiMap", lambda _: DiscreteWrapper(MultiMapEnv()))
+
+    csv_path = "searches/results.csv"
+    starting_idx = 0
+    if os.path.exists(csv_path):
+        with open(csv_path, mode="r") as f:
+            starting_idx = len(f.readlines())
+
+    for search_idx in trange(args.n_searches, desc="Searches"):
+        config = {
             "framework": "torch",
             "model": {
                 "custom_model": "image-dqn",
@@ -127,107 +191,41 @@ def train_model(args):
             "hiddens": [],
             # Don't save experiences.
             # "output": None,
-            # "compress_observations": True,
+            "compress_observations": True,
             "num_workers": 0,
-            "num_gpus": 0.5,
+            "num_gpus": 0.75,
             "rollout_fragment_length": 50,
+            "log_level": "ERROR",
         }
-    )
 
-    # Start training from a checkpoint, if available.
-    if args.model_path:
-        trainer.restore(args.model_path)
+        additional_params = {}
 
-    plot = plotter.Plotter('dqn_agent')
-    for i in range(args.epochs):  # Number of episodes (basically epochs)
-        print(f'----------------------- Starting epoch {i} ----------------------- ')
-        # train() trains only a single episode
-        result = trainer.train()
-        print(result)
-        plot.add_results(result)
+        lr = 10 ** np.random.uniform(-5, -3.25)
+        additional_params["learning_starts"] = np.random.choice(list(range(100, 1001, 100)))
+        additional_params["grad_clip"] = np.random.choice([None, 10, 20, 30, 40, 50, 60, 80])
+        # additional_params["dueling"] = np.random.choice([True, False])
+        additional_params["lr"] = lr
+        additional_params["lr_schedule"] = [(0, lr), (args.epochs * config["buffer_size"], lr / 1000.)]
 
-        # Save model so far.
-        checkpoint_path = trainer.save()
-        print(f'Epoch {i}, checkpoint saved at: {checkpoint_path}')
+        for k, v in additional_params.items():
+            config[k] = v
 
-        # Cleanup CUDA memory to reduce memory usage.
-        torch.cuda.empty_cache()
-        # Debug log to monitor memory.
-        print(torch.cuda.memory_summary(device=None, abbreviated=False))
+        best_mean_reward, best_epoch, best_path = train_model(args, config)
 
-    plot.plot('DQN DuckieTown-MultiMap')
+        additional_params["best_mean_reward"] = best_mean_reward
+        additional_params["best_epoch"] = best_epoch
+        additional_params["best_path"] = best_path
 
+        # No need to save lr_schedule, since it's deterministic on lr
+        del additional_params["lr_schedule"]
 
-def evaluate_model(args):
-    if args.model_path == '':
-        print('Cannot evaluate model, no --model_path set')
-        exit(1)
-
-    def get_env():
-        # Simulator env uses a single map, so better for evaluation/testing.
-        # DiscreteWrapper just converts wheel velocities to high level discrete actions.
-        return DiscreteWrapper(simulator.Simulator(
-            map_name=args.map,
-            max_steps=2000,
-        ))
-
-    # Rather than reuse the env, another one is created later because I can't
-    # figure out how to provide register_env with an object, th
-    register_env('DuckieTown-Simulator', lambda _: get_env())
-    trainer = DQNTrainer(
-        env="DuckieTown-Simulator",
-        config={
-            "framework": "torch",
-            "model": {
-                "custom_model": "image-dqn",
-            },
-            # Dueling off
-            "dueling": False,
-            # No hidden layers
-            "hiddens": [],
-        },
-    )
-    trainer.restore(args.model_path)
-
-    sim_env = get_env()
-
-    # Standard OpenAI Gym reset/action/step/render loop.
-    # This matches how the `enjoy_reinforcement.py` script works, see: https://git.io/J3js2
-    done = False
-    observation = sim_env.reset()
-    episode_reward = 0
-    while not done:
-        action = trainer.compute_action(observation)
-        observation, reward, done, _ = sim_env.step(action)
-        episode_reward += reward
-        sim_env.render()
-
-    print(f'Episode complete, total reward: {episode_reward}')
+        params_for_df = {}
+        for k, v in additional_params.items():
+            params_for_df[k] = [v]
 
 
-def get_parser():
-    parser = argparse.ArgumentParser()
-
-    # Training options
-    parser.add_argument('--epochs', default=10, type=int, help='Num of epochs to train for')
-
-    # Evaluation options
-    parser.add_argument('--eval', action='store_true', default=False, help='Evaluate model instead of train')
-    parser.add_argument('--map', default='loop_empty', help='Which map to evaluate on, see https://git.io/J3jES')
-    # Looks like, e.g. `DQN_DuckieTown-MultiMap_2021-05-10_20-43-32ndgfiq44/checkpoint_000009/checkpoint-9`
-    parser.add_argument('--model_path', default='', help='Location to pre-trained model')
-
-    return parser.parse_args()
-
-
-if __name__ == '__main__':
-    args = get_parser()
-
-    # Start ray
-    ray.init()
-    ModelCatalog.register_custom_model("image-dqn", RLLibDQNCritic)
-
-    if args.eval:
-        evaluate_model(args)
-    else:
-        train_model(args)
+        pd.DataFrame.from_dict(params_for_df).to_csv(
+            csv_path,
+            mode="a" if os.path.exists(csv_path) else "w",
+            header=not os.path.exists(csv_path)
+        )
